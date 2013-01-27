9cc7e20776900664548bc4aeb40ac052afe5bcff
colin-scott <cs@cs.berkeley.edu>
2013-01-26 22:08:39 -0800

    Add debugger branch version of l2_multi bug

---
+++ loop_debugger_branch/mcs_config.py
@@ -0,0 +1,18 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow import EfficientMCSFinder
+from sts.invariant_checker import InvariantChecker
+from sts.simulation_state import SimulationConfig
+from orig_config import my_funky_invariant_check
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdline='./pox.py --verbose sts.syncproto.pox_syncer --blocking=False openflow.discovery forwarding.l2_multi sts.util.socket_mux.pox_monkeypatcher openflow.of_01 --address=__address__ --port=__port__', address='127.0.0.1', port=6633, cwd='pox', sync='tcp:localhost:18899')],
+                 topology_class=MeshTopology,
+                 topology_params="num_switches=2",
+                 patch_panel_class=BufferedPatchPanel,
+                 dataplane_trace="dataplane_traces/ping_pong_same_subnet.trace",
+                 multiplex_sockets=True)
+
+control_flow = EfficientMCSFinder(simulation_config, "exp/fuzz_pox_mesh/events.trace",
+                                  invariant_check=my_funky_invariant_check,
+                                  wait_on_deterministic_values=False)
---
+++ loop_debugger_branch/orig_config.py
@@ -0,0 +1,46 @@
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import MeshTopology
+from sts.control_flow import Fuzzer, Interactive
+from sts.input_traces.input_logger import InputLogger
+from sts.invariant_checker import InvariantChecker
+from sts.simulation_state import SimulationConfig
+
+# Use POX as our controller
+command_line = ('''./pox.py --verbose '''
+                '''sts.syncproto.pox_syncer --blocking=False '''
+                #'''openflow.mock_discovery forwarding.l2_multi '''
+                '''openflow.discovery forwarding.l2_multi '''
+                '''sts.util.socket_mux.pox_monkeypatcher '''
+                '''openflow.of_01 --address=__address__ --port=__port__''')
+controllers = [ControllerConfig(command_line, cwd="pox", sync="tcp:localhost:18899")]
+topology_class = MeshTopology
+topology_params = "num_switches=2"
+dataplane_trace = "dataplane_traces/ping_pong_same_subnet.trace"
+
+simulation_config = SimulationConfig(controller_configs=controllers,
+                                     topology_class=topology_class,
+                                     topology_params=topology_params,
+                                     dataplane_trace=dataplane_trace,
+                                     multiplex_sockets=True)
+
+def my_funky_invariant_check(simulation):
+  from sts.invariant_checker import InvariantChecker
+  result = InvariantChecker.check_loops(simulation)
+  if result:
+    return result
+  result = InvariantChecker.check_connectivity(simulation)
+  if not result:
+    print "Connectivity established - bailing out"
+    import sys
+    sys.exit(0)
+  return []
+
+control_flow = Fuzzer(simulation_config, check_interval=20,
+                      #mock_link_discovery=True,
+                      halt_on_violation=True,
+                      input_logger=InputLogger(),
+                      invariant_check=my_funky_invariant_check,
+                      steps=141,
+                      #random_seed=466448715
+                      )
+#control_flow = Interactive(simulation_config, input_logger=InputLogger())
