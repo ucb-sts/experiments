199a9409d8554aca6b816a6a5d303e08f245f3bf
Andreas Wundsam <b-github.com@wundsam.net>
2013-01-27 20:43:40 -0800

    add floodlight fuzzing configs

---
+++ config/fuzzer_params_no_failure.py
@@ -0,0 +1,13 @@
+switch_failure_rate = 0.00
+switch_recovery_rate = 0.5
+dataplane_drop_rate = 0.0
+dataplane_delay_rate = 0.0
+controlplane_block_rate = 0.0
+controlplane_unblock_rate = 1.0
+ofp_message_receipt_rate = 1.0
+link_failure_rate = 0.00
+link_recovery_rate = 0.50
+controller_crash_rate = 0.0
+controller_recovery_rate = 0.0
+traffic_generation_rate = 0.2
+host_migration_rate = 0.00
