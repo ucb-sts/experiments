db4446d79725580aa87ee7f92b4aca667a9b7692
Sam Whitlock <phynominal@gmail.com>
2013-02-01 09:15:16 -0800

    configuring for mcs run

--- fuzz_pox_4mesh_blackhole/mcs_config.py
+++ fuzz_pox_4mesh_blackhole/mcs_config.py
@@ -1,9 +1,9 @@
-
 from config.experiment_config_lib import ControllerConfig
 from sts.topology import *
 from sts.control_flow import EfficientMCSFinder
 from sts.invariant_checker import InvariantChecker
 from sts.simulation_state import SimulationConfig
+from orig_config import check_stale_entries
 
 simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdline='./pox.py --verbose --no-cli sts.syncproto.pox_syncer openflow.discovery openflow.spanning_tree forwarding.l2_multi sts.util.socket_mux.pox_monkeypatcher openflow.of_01 --address=__address__ --port=__port__', address='127.0.0.1', port=6633, cwd='pox', sync='tcp:localhost:18900')],
                  topology_class=MeshTopology,
@@ -13,4 +13,5 @@ simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdlin
                  multiplex_sockets=True)
 
 control_flow = EfficientMCSFinder(simulation_config, "exp/fuzz_pox_4mesh_blackhole/events.trace",
+                                  invariant_check=check_stale_entries,
                                   wait_on_deterministic_values=False)
