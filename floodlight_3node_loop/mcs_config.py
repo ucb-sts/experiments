d98741548e9fa2aaf585c4911460368bb788be08
Andreas Wundsam <b-github.com@wundsam.net>
2013-01-31 23:18:33 -0800

    fix mcs_config

--- floodlight_3node_loop/mcs_config.py
+++ floodlight_3node_loop/mcs_config.py
@@ -1,16 +1,37 @@
+from exp.floodlight_3node_loop.orig_config import my_funky_invariant_check
+
+from config.experiment_config_lib import ControllerConfig, find_ports
 
 from config.experiment_config_lib import ControllerConfig
 from sts.topology import *
-from sts.control_flow import EfficientMCSFinder
-from sts.invariant_checker import InvariantChecker
+from sts.control_flow import *
 from sts.simulation_state import SimulationConfig
 
-simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdline='java  -server -Xmx512m -Xms512m -Xmn128m -XX:+UseParallelGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -XX:MaxInlineSize=8192 -XX:FreqInlineSize=8192 -XX:CompileThreshold=1500 -XX:PreBlockSpin=8 -Dpython.security.respectJavaAccessibility=false -Dlogback.configurationFile=logback.xml -jar target/floodlight.jar -cf __config__', address='127.0.0.1', port=6654, cwd='floodlight')],
+jvm_opts = ""
+jvm_opts += " -server"
+#jvm_opts += " -Xmx2g -Xms2g -Xmn800m"
+jvm_opts += " -Xmx512m -Xms512m -Xmn128m"
+jvm_opts += " -XX:+UseParallelGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods"
+jvm_opts += " -XX:MaxInlineSize=8192 -XX:FreqInlineSize=8192"
+jvm_opts += " -XX:CompileThreshold=1500 -XX:PreBlockSpin=8"
+jvm_opts += " -Dpython.security.respectJavaAccessibility=false"
+log_config = "-Dlogback.configurationFile=logback.xml"
+command_line = ("java %s %s -jar target/floodlight.jar -cf __config__" % (jvm_opts, log_config))
+
+timestamp_results = True
+
+additional_ports = find_ports(of=range(6633,6833), rest=range(8080, 8280), jython=range(7655, 7855))
+controllers = [ ControllerConfig(command_line, cwd="floodlight", port=additional_ports['of'], additional_ports = additional_ports, uuid=("127.0.0.1", 6633), config_template="exp/config/floodlightconfig.properties.template") ]
+
+simulation_config = SimulationConfig(controller_configs=controllers,
                  topology_class=MeshTopology,
                  topology_params="num_switches=3",
                  patch_panel_class=BufferedPatchPanel,
-                 dataplane_trace="exp/fuzz_floodlight_mesh_3node_2013_01_27_22_48_07/dataplane.trace",
+                 dataplane_trace="exp/floodlight_3node_loop/dataplane.trace",
                  multiplex_sockets=False)
 
-control_flow = EfficientMCSFinder(simulation_config, "exp/fuzz_floodlight_mesh_3node_2013_01_27_22_48_07/events.trace",
-                                  wait_on_deterministic_values=False)
+
+control_flow = EfficientMCSFinder(simulation_config, "exp/floodlight_3node_loop/events.trace", epsilon_seconds=0.1,
+                                    invariant_check=my_funky_invariant_check,
+                                    no_violation_verification_runs=15,
+                                    wait_on_deterministic_values=False)
