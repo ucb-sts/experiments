5c6929d58d9ceaccb055fd28eb42f0f612c9ad55
Sam Whitlock <phynominal@gmail.com>
2013-01-31 22:13:15 -0800

    code reuse for mcs config

--- fuzz_pox_fattree/mcs_config.py
+++ fuzz_pox_fattree/mcs_config.py
@@ -4,7 +4,7 @@ from sts.topology import *
 from sts.control_flow import EfficientMCSFinder
 from sts.invariant_checker import InvariantChecker
 from sts.simulation_state import SimulationConfig
-from replay_config import check_stale_entries
+from orig_config import check_stale_entries
 
 simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdline='./pox.py --verbose --no-cli sts.syncproto.pox_syncer openflow.discovery openflow.spanning_tree forwarding.l2_multi sts.util.socket_mux.pox_monkeypatcher openflow.of_01 --address=__address__ --port=__port__', address='127.0.0.1', port=6633, cwd='pox', sync='tcp:localhost:18900')],
                  topology_class=FatTree,
@@ -14,5 +14,5 @@ simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdlin
                  multiplex_sockets=True)
 
 control_flow = EfficientMCSFinder(simulation_config, "exp/fuzz_pox_fattree/events.trace",
-                                  invariant_checker=check_stale_entries,
+                                  invariant_check=check_stale_entries,
                                   wait_on_deterministic_values=False)
