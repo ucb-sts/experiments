3651e078608a3eb6d73155ffdcf2aef3c33ded85
Sam Whitlock <phynominal@gmail.com>
2013-02-01 09:35:09 -0800

    changing to avoid collision

--- fuzz_pox_4mesh_blackhole/mcs_config.py
+++ fuzz_pox_4mesh_blackhole/mcs_config.py
@@ -1,3 +1,4 @@
+
 from config.experiment_config_lib import ControllerConfig
 from sts.topology import *
 from sts.control_flow import EfficientMCSFinder
@@ -5,7 +6,7 @@ from sts.invariant_checker import InvariantChecker
 from sts.simulation_state import SimulationConfig
 from orig_config import check_stale_entries
 
-simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdline='./pox.py --verbose --no-cli sts.syncproto.pox_syncer openflow.discovery openflow.spanning_tree forwarding.l2_multi sts.util.socket_mux.pox_monkeypatcher openflow.of_01 --address=__address__ --port=__port__', address='127.0.0.1', port=6633, cwd='pox', sync='tcp:localhost:18900')],
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdline='./pox.py --verbose --no-cli sts.syncproto.pox_syncer openflow.discovery openflow.spanning_tree forwarding.l2_multi sts.util.socket_mux.pox_monkeypatcher openflow.of_01 --address=__address__ --port=__port__', address='127.0.0.1', port=6632, cwd='pox', sync='tcp:localhost:18901')],
                  topology_class=MeshTopology,
                  topology_params="num_switches=4",
                  patch_panel_class=BufferedPatchPanel,
