03a3411fe9ccee85ac1dfc96b7034ba745a26e47
Sam Whitlock <phynominal@gmail.com>
2013-01-31 22:14:28 -0800

    deprecating old replay config

--- fuzz_pox_fattree/replay_config.py
+++ fuzz_pox_fattree/replay_config.py
@@ -11,29 +11,5 @@ simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdlin
                  dataplane_trace="exp/fuzz_pox_fattree/dataplane.trace",
                  multiplex_sockets=True)
 
-old_ingress_dpid = 3
-def check_stale_entries(simulation):
-  access_links = filter(lambda x: x.switch.dpid == old_ingress_dpid,
-                        simulation.topology.access_links)
-  assert(len(access_links) < 2)
-  if len(access_links) == 1:
-    link = access_links[0]
-    old_switch = link.switch
-    old_port = link.switch_port # ofp_phy_port
-    old_port_no = old_port.port_no
-
-    # Conjunction of old_port in switch's down ports
-    # and old_port still being in routing table
-    port_down = old_port_no in old_switch.down_port_nos
-    old_entries = old_switch.table.entries_for_port(old_port_no)
-    old_old_entries = self.fuzzer.old_routing_entries
-
-    if port_down and len(old_entries) > 0:
-      # we have a violation!
-      return old_entries
-
-  return []
-
 control_flow = Replayer(simulation_config, "exp/fuzz_pox_fattree/events.trace",
-                        invariant_checker=check_stale_entries,
                         wait_on_deterministic_values=False)
