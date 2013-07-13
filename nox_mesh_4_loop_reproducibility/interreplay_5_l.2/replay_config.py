0c6a5d8268db29ef6dd7ed3e677085f6d14058ee
colin-scott <cs@cs.berkeley.edu>
2013-07-13 10:21:11 -0700

    nox_mesh_4_loop_reproducibility is the MCS of the nox 4 loop that does not reproduce the bug.

---
+++ nox_mesh_4_loop_reproducibility/interreplay_5_l.2/replay_config.py
@@ -0,0 +1,15 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow import Replayer
+from sts.simulation_state import SimulationConfig
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='./nox_core -v -i ptcp:6635 routing', address='127.0.0.1', port=6635, cwd='nox_classic/build/src')],
+                 topology_class=MeshTopology,
+                 topology_params="num_switches=4",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=False)
+
+control_flow = Replayer(simulation_config, "experiments/nox_mesh_4_loop_reproducibility/interreplay_5_l.2/events.trace",
+                        wait_on_deterministic_values=False)
+# Invariant check: 'None'
