9f307409c80c8cf2ee078463ba1870b3f7db77ba
Andrew Or <andrewor14@gmail.com>
2013-07-19 10:50:44 -0700

    Floodlight 3 node loop FINAL MCS, aka Zeta

---
+++ zeta_final/interreplay_85_r.3/replay_config.py
@@ -0,0 +1,15 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow import Replayer
+from sts.simulation_state import SimulationConfig
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='java  -server -Xmx512m -Xms512m -Xmn128m -XX:+UseParallelGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -XX:MaxInlineSize=8192 -XX:FreqInlineSize=8192 -XX:CompileThreshold=1500 -XX:PreBlockSpin=8 -Dpython.security.respectJavaAccessibility=false -Dlogback.configurationFile=logback.xml -jar target/floodlight.jar -cf __config__', address='127.0.0.1', port=6815, cwd='floodlight')],
+                 topology_class=MeshTopology,
+                 topology_params="num_switches=3",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=False)
+
+control_flow = Replayer(simulation_config, "experiments/zeta_mcs_10_2013_07_18_16_36_18/interreplay_85_r.3/events.trace",
+                        wait_on_deterministic_values=False)
+# Invariant check: 'None'
