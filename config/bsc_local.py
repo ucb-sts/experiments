d8d9c62ea25f0ee520920a2679437da77545ac05
Andrew Or <andrewor14@gmail.com>
2013-08-07 15:20:06 -0700

    New way of killing/restarting Big Switch Controllers

--- config/bsc_local.py
+++ config/bsc_local.py
@@ -7,25 +7,26 @@ from sts.simulation_state import SimulationConfig
 # Work directory must be absolute path
 work_directory = "/home-local/andrewor/work"
 start_cmd = "./start_vms -d %s" % work_directory
-kill_cmd = "./power_off_vm -d %s -i 1" % work_directory
-restart_cmd = "./power_on_vm -d %s -i 1" % work_directory
+kill_cmd = "./stop_process -d %s -i %s" % (work_directory, "%d")
+restart_cmd = "./start_process -d %s -i %s" % (work_directory, "%d")
 get_address_cmd = "./show_vms -d %s" % work_directory
 dummy_cmd = "sleep 1" 
 
 # Use Floodlight as our controller
-controllers = [ ControllerConfig(start_cmd, kill_cmd, restart_cmd, cwd="experiments/scripts/bsc", address="__address__", port=6633, controller_type="bsc", label="c1", get_address_cmd=get_address_cmd) ] 
+controllers = [ ControllerConfig(start_cmd, cwd="experiments/scripts/bsc", address="__address__", port=6633, controller_type="bsc", label="c1", kill_cmd=kill_cmd % 1, restart_cmd=restart_cmd % 1,  get_address_cmd=get_address_cmd), 
+                ControllerConfig(dummy_cmd, cwd="experiments/scripts/bsc", address="__address__", port=6633, controller_type="bsc", label="c2", kill_cmd=kill_cmd % 2, restart_cmd=restart_cmd % 2,  get_address_cmd=get_address_cmd)]
 topology_class = MeshTopology
 topology_params = "num_switches=3"
 
 simulation_config = SimulationConfig(controller_configs=controllers,
                                      topology_class=topology_class,
-                                     topology_params=topology_params,
+                                     topology_params=topology_params
                                      )
 
-control_flow = Fuzzer(simulation_config, check_interval=20,
+control_flow = Fuzzer(simulation_config, check_interval=5,
                       halt_on_violation=True,
                       input_logger=InputLogger(),
-                      invariant_check_name="InvariantChecker.check_liveness",
+                      invariant_check_name="check_everything",
                       steps=999,
                       fuzzer_params="experiments/config/fuzzer_params_heavy_failures.py"
                       )
--- config/bsc_remote_ha.py
+++ config/bsc_remote_ha.py
@@ -7,14 +7,14 @@ from sts.simulation_state import SimulationConfig
 # Work directory must be absolute path
 work_directory = "/home-local/andrewor/work"
 start_cmd = "./start_vms -d %s -h -r" % work_directory
-kill_cmd = "./power_off_vm -d %s -i %s -r" % (work_directory, "%d")
-restart_cmd = "./power_on_vm -d %s -i %s -r" % (work_directory, "%d")
+kill_cmd = "./stop_process -d %s -i %s -r" % (work_directory, "%d")
+restart_cmd = "./start_process -d %s -i %s -r" % (work_directory, "%d")
 get_address_cmd = "./show_vms -d %s -r" % work_directory
 dummy_cmd = "sleep 1" 
 
 # Use Floodlight as our controller
-controllers = [ ControllerConfig(start_cmd, kill_cmd % 1, restart_cmd % 1, cwd="experiments/scripts/bsc", address="__address__", port=6633, controller_type="bsc", label="c1", get_address_cmd=get_address_cmd), 
-                ControllerConfig(dummy_cmd, kill_cmd % 2, restart_cmd % 2, cwd="experiments/scripts/bsc", address="__address__", port=6633, controller_type="bsc", label="c2", get_address_cmd=get_address_cmd)]
+controllers = [ ControllerConfig(start_cmd, cwd="experiments/scripts/bsc", address="__address__", port=6633, controller_type="bsc", label="c1", kill_cmd=kill_cmd % 1, restart_cmd=restart_cmd % 1,  get_address_cmd=get_address_cmd), 
+                ControllerConfig(dummy_cmd, cwd="experiments/scripts/bsc", address="__address__", port=6633, controller_type="bsc", label="c2", kill_cmd=kill_cmd % 2, restart_cmd=restart_cmd % 2,  get_address_cmd=get_address_cmd)]
 topology_class = MeshTopology
 topology_params = "num_switches=3"
 
