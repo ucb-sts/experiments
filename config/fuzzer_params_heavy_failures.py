8e2e50138201764374f9f690fd77f8385dd1e29c
Andrew Or <andrewor14@gmail.com>
2013-10-15 19:37:38 -0700

    Add passthrough flag to fuzzer params

--- config/fuzzer_params_heavy_failures.py
+++ config/fuzzer_params_heavy_failures.py
@@ -6,6 +6,7 @@ controlplane_block_rate = 0.1
 controlplane_unblock_rate = 1.0
 ofp_message_receipt_rate = 1.0
 ofp_message_send_rate = 1.0
+ofp_cmd_passthrough_rate = 0.0
 link_failure_rate = 0.02
 link_recovery_rate = 0.50
 controller_crash_rate = 0.0
