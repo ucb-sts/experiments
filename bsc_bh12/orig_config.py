b663e45f0db7a4349985a82cea8e24dab682bbda
Andrew Or <andrewor14@gmail.com>
2013-10-15 19:37:06 -0700

    Remove outdated BSC fuzzing

--- bsc_bh12/orig_config.py
+++
@@ -1,9 +0,0 @@
-from config.experiment_config_lib import ControllerConfig
-from sts.topology import MeshTopology
-from sts.control_flow import Fuzzer, Interactive
-from sts.input_traces.input_logger import InputLogger
-from sts.simulation_state import SimulationConfig
-
-# Work directory must be absolute path
-work_directory = "/home-local/andrewor/work"
-start_cmd = "./start_vms -d  -h -r" 
