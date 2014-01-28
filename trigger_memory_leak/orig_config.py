d2ac2f06a4c7e64205f61dc941c7467f7a885c61
colin-scott <cs@cs.berkeley.edu>
2014-01-27 18:09:32 -0800

    Synthetic memory leak?

---
+++ trigger_memory_leak/orig_config.py
@@ -0,0 +1,31 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import MeshTopology
+from sts.control_flow import Fuzzer
+from sts.input_traces.input_logger import InputLogger
+from sts.simulation_state import SimulationConfig
+
+# Use POX as our controller
+start_cmd = ('''./pox.py --verbose '''
+             #'''sts.syncproto.pox_syncer --blocking=False '''
+             '''openflow.discovery topology '''
+             '''host_tracker '''
+             '''sts.util.socket_mux.pox_monkeypatcher '''
+             '''openflow.of_01 --address=__address__ --port=__port__''')
+
+controllers = [ControllerConfig(start_cmd, cwd="pox")]
+topology_class = MeshTopology
+topology_params = "num_switches=2"
+
+simulation_config = SimulationConfig(controller_configs=controllers,
+                                     topology_class=topology_class,
+                                     topology_params=topology_params,
+                                     multiplex_sockets=True)
+
+control_flow = Fuzzer(simulation_config, check_interval=150,
+                      halt_on_violation=True,
+                      initialization_rounds=30,
+                      input_logger=InputLogger(),
+                      invariant_check_name="InvariantChecker.check_liveness")
+
+raise RuntimeError("Please add this parameter to Fuzzer: fuzzer_params='experiments/trigger_memory_leak/fuzzer_params.py'")
