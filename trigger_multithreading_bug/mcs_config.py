bbd81e2d1b8a7f6ce178c0a353b9af7527ac6c1f
colin-scott <cs@cs.berkeley.edu>
2014-01-29 16:57:29 -0800

    Latest mcs config

--- trigger_multithreading_bug/mcs_config.py
+++ trigger_multithreading_bug/mcs_config.py
@@ -5,7 +5,8 @@ from sts.control_flow import EfficientMCSFinder
 from sts.invariant_checker import InvariantChecker
 from sts.simulation_state import SimulationConfig
 
-simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='./pox.py --verbose openflow.discovery forwarding.topo_proactive sts.util.socket_mux.pox_monkeypatcher openflow.of_01 --address=__address__ --port=__port__', label='c1', address='127.0.0.1', cwd='dart_pox')],
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='./pox.py --verbose openflow.discovery forwarding.topo_proactive sts.util.socket_mux.pox_monkeypatcher openflow.of_01 --address=__address__ --port=8901',
+    port=8901, label='c1', address='127.0.0.1', cwd='dart_pox')],
                  topology_class=MeshTopology,
                  topology_params="num_switches=10",
                  patch_panel_class=BufferedPatchPanel,
@@ -17,6 +18,6 @@ control_flow = EfficientMCSFinder(simulation_config, "experiments/trigger_multit
                                   default_dp_permit=True,
                                   pass_through_whitelisted_messages=True,
                                   delay_flow_mods=False,
-                                  max_replays_per_subsequence=20,
+                                  max_replays_per_subsequence=2,
                                   invariant_check_name='InvariantChecker.check_liveness',
                                   bug_signature="")
