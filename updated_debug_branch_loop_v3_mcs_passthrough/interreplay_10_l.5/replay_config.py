575526baa4ea451bdf7673b68579348024ed97e6
Jefferson Lai <jefflai2@gmail.com>
2014-03-25 02:32:10 -0700

    Added passthrough experiments for updated_debug_branch_loop_v3_mcs

---
+++ updated_debug_branch_loop_v3_mcs_passthrough/interreplay_10_l.5/replay_config.py
@@ -0,0 +1,15 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow import Replayer
+from sts.simulation_state import SimulationConfig
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='./pox.py --verbose sts.syncproto.pox_syncer --blocking=False openflow.discovery forwarding.l2_multi sts.util.socket_mux.pox_monkeypatcher openflow.of_01 --address=__address__ --port=__port__', address='127.0.0.1', port=6633, cwd='pox', sync='tcp:localhost:18899')],
+                 topology_class=MeshTopology,
+                 topology_params="num_switches=2",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=True)
+
+control_flow = Replayer(simulation_config, "experiments/updated_debug_branch_loop_v3_mcs_passthrough/interreplay_10_l.5/events.trace",
+                        wait_on_deterministic_values=False)
+# Invariant check: 'None'
