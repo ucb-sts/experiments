210d616689f3556b82a61b7a89d42ace62e8a693
Jefferson Lai <jefflai2@gmail.com>
2014-04-20 12:57:10 -0700

    POX Migration Blackhole w/ passthrough

---
+++ fuzz_pox_4mesh_blackhole_mcs_passthrough/intermcs_2_r.0/replay_config.py
@@ -0,0 +1,15 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow import Replayer
+from sts.simulation_state import SimulationConfig
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdline='./pox.py --verbose --no-cli sts.syncproto.pox_syncer openflow.discovery openflow.spanning_tree forwarding.l2_multi sts.util.socket_mux.pox_monkeypatcher openflow.of_01 --address=__address__ --port=__port__', address='127.0.0.1', port=6632, cwd='pox', sync='tcp:localhost:18901')],
+                 topology_class=MeshTopology,
+                 topology_params="num_switches=4",
+                 patch_panel_class=BufferedPatchPanel,
+                 dataplane_trace="experiments/fuzz_pox_4mesh_blackhole/dataplane.trace",
+                 multiplex_sockets=True)
+
+control_flow = Replayer(simulation_config, "experiments/fuzz_pox_4mesh_blackhole_mcs_passthrough/intermcs_2_r.0/events.trace",
+                        wait_on_deterministic_values=False)
---
+++ fuzz_pox_4mesh_blackhole_mcs_passthrough_10replays/orig_config.py
@@ -0,0 +1,34 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow import EfficientMCSFinder
+from sts.invariant_checker import InvariantChecker
+from sts.simulation_state import SimulationConfig
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdline='./pox.py --verbose --no-cli sts.syncproto.pox_syncer openflow.discovery openflow.spanning_tree forwarding.l2_multi sts.util.socket_mux.pox_monkeypatcher openflow.of_01 --address=__address__ --port=__port__', address='127.0.0.1', port=6632, cwd='pox', sync='tcp:localhost:18901')],
+                 topology_class=MeshTopology,
+                 topology_params="num_switches=4",
+                 patch_panel_class=BufferedPatchPanel,
+                 dataplane_trace="experiments/fuzz_pox_4mesh_blackhole/dataplane.trace",
+                 multiplex_sockets=True)
+
+def check_stale_entries(simulation):
+  '''Check that the migrated host's old switch has stale entries'''
+  dpid = 1 # hardcoding host 1
+  port_no = 4
+
+  switch = simulation.topology.get_switch(dpid)
+
+  port_down = port_no in switch.down_port_nos
+  old_entries = switch.table.entries_for_port(port_no)
+
+  if port_down and len(old_entries) > 0:
+    # we have a violation!
+    return old_entries
+
+  return []
+
+control_flow = EfficientMCSFinder(simulation_config, "experiments/fuzz_pox_4mesh_blackhole/events.trace",
+                                  invariant_check=check_stale_entries,
+                                  max_replays_per_subsequence=10,
+                                  wait_on_deterministic_values=False)
