9a0e741cd124dc374a763536c1ffc779306997d4
Jefferson Lai <jefflai2@gmail.com>
2014-04-22 21:19:24 -0700

    load_balancer_fuzzer_mcs passthrough

---
+++ load_balancer_fuzzer_mcs_passthrough_5replays/interreplay_388_l_3/openflow_replay_config.py
@@ -0,0 +1,19 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow import OpenFlowReplayer
+from sts.simulation_state import SimulationConfig
+from sts.input_traces.input_logger import InputLogger
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='./pox.py --verbose --unthreaded-sh misc.ip_loadbalancer --ip=10.1.3.2 --servers=10.1.3.2,10.2.3.2 sts.util.socket_mux.pox_monkeypatcher   openflow.discovery openflow.of_01 --address=__address__ --port=__port__', label='c1', address='127.0.0.1', cwd='dart_pox')],
+                 topology_class=MeshTopology,
+                 topology_params="num_switches=3",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=True,
+                 kill_controllers_on_exit=True)
+
+control_flow = OpenFlowReplayer(simulation_config, "experiments/load_balancer_fuzzer_mcs_passthrough_5replays/interreplay_388_l_3/events.trace")
+# wait_on_deterministic_values=False
+# delay_flow_mods=False
+# Invariant check: 'check_for_ofp_error'
+# Bug signature: "ERROR_SENT"
