0418c496ae9454423ede4211a455adec24dc3459
colin-scott <cs@cs.berkeley.edu>
2014-04-22 18:40:46 -0700

    Add Andi's missing original trace for early_packet_in

---
+++ pox_switch_hash_bug_fuzz_fuzzer/fuzzer_params.py
@@ -0,0 +1,17 @@
+switch_failure_rate = 0.000
+switch_recovery_rate = 0.5
+dataplane_drop_rate = 0.00
+dataplane_delay_rate = 0.0
+controlplane_block_rate = 0.00
+controlplane_unblock_rate = 0.05
+ofp_message_receipt_rate = 1.0
+ofp_message_send_rate = 1.0
+ofp_cmd_passthrough_rate = 0.0
+link_failure_rate = 0.1
+link_recovery_rate = 0.9
+controller_crash_rate = 0.0
+controller_recovery_rate = 1.0
+traffic_generation_rate = 0.0
+host_migration_rate = 0.00
+intracontroller_block_rate = 0.00
+intracontroller_unblock_rate = 0.0
