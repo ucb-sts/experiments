5b477977d93978763e46581a76e5eaf320e1041b
colin-scott <cs@cs.berkeley.edu>
2014-05-25 14:54:52 -0700

    Add possible re-run of in-flight blackhole. This one is a regular blackhole persistent after 21 rounds

---
+++ retrigger_debug_branch_loop/mcs_config.py
@@ -0,0 +1,22 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow.mcs_finder import EfficientMCSFinder
+from sts.invariant_checker import InvariantChecker
+from sts.simulation_state import SimulationConfig
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='./pox.py --verbose sts.syncproto.pox_syncer --blocking=False openflow.discovery forwarding.l2_multi sts.util.socket_mux.pox_monkeypatcher openflow.of_01 --address=__address__ --port=__port__', label='c1', address='127.0.0.1', cwd='old_pox', sync='tcp:localhost:18899')],
+                 topology_class=MeshTopology,
+                 topology_params="num_switches=2",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=True,
+                 ignore_interposition=False,
+                 kill_controllers_on_exit=True)
+
+control_flow = EfficientMCSFinder(simulation_config, "experiments/retrigger_debug_branch_loop/events.trace",
+                                  wait_on_deterministic_values=False,
+                                  default_dp_permit=False,
+                                  pass_through_whitelisted_messages=False,
+                                  delay_flow_mods=False,
+                                  invariant_check_name='check_for_blackholes_or_connectivity',
+                                  bug_signature="")
