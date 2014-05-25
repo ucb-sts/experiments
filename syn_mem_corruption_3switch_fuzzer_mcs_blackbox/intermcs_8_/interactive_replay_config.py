ce6821fed1ae48e080b93596daad689e6aae4f65
colin-scott <cs@cs.berkeley.edu>
2014-05-25 15:44:40 -0700

    Add blackbox version

---
+++ syn_mem_corruption_3switch_fuzzer_mcs_blackbox/intermcs_8_/interactive_replay_config.py
@@ -0,0 +1,20 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow import InteractiveReplayer
+from sts.simulation_state import SimulationConfig
+from sts.input_traces.input_logger import InputLogger
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='./pox.py --verbose openflow.of_01 --address=__address__ --port=__port__ openflow.discovery forwarding.l2_multi_syn_mem_corruption', label='c1', address='127.0.0.1', cwd='pox')],
+                 topology_class=MeshTopology,
+                 topology_params="num_switches=4",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=False,
+                 ignore_interposition=True,
+                 kill_controllers_on_exit=True)
+
+control_flow = InteractiveReplayer(simulation_config, "experiments/syn_mem_corruption_3switch_fuzzer_mcs_blackbox/intermcs_8_/mcs.trace.notimeouts")
+# wait_on_deterministic_values=False
+# delay_flow_mods=False
+# Invariant check: 'InvariantChecker.check_liveness'
+# Bug signature: "c1"
