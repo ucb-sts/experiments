fc7d135f333e53340816b34156800b518e66398f
colin-scott <cs@cs.berkeley.edu>
2014-05-29 22:46:34 -0700

    Add blackbox zeta uns

---
+++ zeta_final_blackbox_2014_05_29_22_33_49/interreplay_0_reproducibility/replay_config.py
@@ -0,0 +1,15 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow import Replayer
+from sts.simulation_state import SimulationConfig
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(start_cmd='java  -server -Xmx512m -Xms512m -Xmn128m -XX:+UseParallelGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -XX:MaxInlineSize=8192 -XX:FreqInlineSize=8192 -XX:CompileThreshold=1500 -XX:PreBlockSpin=8 -Dpython.security.respectJavaAccessibility=false -Dlogback.configurationFile=logback.xml -jar target/floodlight.jar -cf __config__', address='127.0.0.1', port=6633, cwd='../floodlight')],
+                 topology_class=MeshTopology,
+                 topology_params="num_switches=3",
+                 patch_panel_class=BufferedPatchPanel,
+                 multiplex_sockets=False)
+
+control_flow = Replayer(simulation_config, "experiments/zeta_final_blackbox_2014_05_29_22_33_49/interreplay_0_reproducibility/events.trace",
+                        wait_on_deterministic_values=False)
+# Invariant check: 'None'
